434a2787e59f71fb9450ab72a7f51de13f9cf77d
Dell <Dell@DESKTOP-CBM6E4G>
2025-07-13 22:02:44 +0300

    qustion 2 3 4

--- task 2/Program.cs
+++ task 2/Program.cs
@@ -5,14 +5,63 @@
         static void Main(string[] args)
         {
             #region question 1 
-            Console.Write("Enter an integer number: ");
-            int number = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Enter a number: ");
+            //int number = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine($"Numbers from 1 to {number}:");
-            for (int i = 1; i <= number; i++)
-            {
-                Console.Write(i + " ");
-            }
+            //Console.WriteLine($"Numbers from 1 to {number}:");
+            //for (int i = 1; i <= number; i++)
+            //{
+            //    Console.Write(i + " ");
+            //}
+
+
+
+            #endregion
+
+            #region question 2 
+
+            //Console.Write("Enter a number: ");
+            //int number = Convert.ToInt32(Console.ReadLine());
+
+            //for (int i = 1; i <= 12; i++)
+            //{
+            //    int result = number * i;
+            //    Console.WriteLine($"{number} x {i} = {result}");
+            //}
+
+
+
+            #endregion
+
+
+            #region question 3
+
+            //Console.Write("Enter a number: ");
+            //int num = Convert.ToInt32(Console.ReadLine());
+
+            //for (int i = 1; i <= num; i++)
+            //{
+            //    if (i % 2 == 0) {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+
+
+            #endregion
+
+            #region question 4
+
+            //Console.Write("Enter first number: ");
+            //int num1 = Convert.ToInt32(Console.ReadLine());
+
+            //Console.Write("Enter second number: ");
+            //int num2 = Convert.ToInt32(Console.ReadLine());
+
+            //int result=1 ;
+
+            //Console.WriteLine($"the number powerd = {result = (num1 ^ num2)}");
+            //msh 3arf a7lha 
             #endregion
         }
     }
