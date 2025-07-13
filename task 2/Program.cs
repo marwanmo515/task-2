bc8ad56728c365db8b467b8cc44678b9088725eb
Dell <Dell@DESKTOP-CBM6E4G>
2025-07-13 19:54:56 +0300

    Add project files.

---
+++ task 2/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace task_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter an integer number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            // Print numbers from 1 to the entered number
+            Console.WriteLine($"Numbers from 1 to {number}:");
+            for (int i = 1; i <= number; i++)
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
